410704ed947b4b9f56795661b8e7ee506b7cfbfa
vitorLostadaC <vitorlostada@hotmail.com>
2023-07-11 22:40:41 -0300

    feat: create user controller

---
+++ backend/src/infra/http/modules/user/user.controller.ts
@@ -0,0 +1,51 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common"
+import { CreateUserUseCase } from "src/modules/users/useCases/createUserUseCase/CreateUserUseCase"
+import { DeleteUserUseCase } from "src/modules/users/useCases/deleteUserUseCase/DeleteUserUseCase"
+import { EditUserUseCase } from "src/modules/users/useCases/editUserUseCase/EditUserUseCase"
+import { FindManyUserUseCase } from "../../../../modules/users/useCases/findManyUseCase/findManyUserUseCase"
+import { CreateUserBody } from "./dtos/CreateUserBody"
+import { EditUserBody } from "./dtos/EditUserBody"
+
+@Controller("user")
+export class UserController {
+  constructor(
+    private createUserUseCase: CreateUserUseCase,
+    private deleteUserUseCase: DeleteUserUseCase,
+    private editUserUseCase: EditUserUseCase,
+    private findManyUserUseCase: FindManyUserUseCase
+  ) {}
+
+  @Post()
+  async createUser(@Body() body: CreateUserBody) {
+    const { email, name, password } = body
+
+    const user = this.createUserUseCase.execute({
+      email,
+      name,
+      password
+    })
+
+    return user
+  }
+
+  @Get()
+  async findManyUser() {
+    const users = this.findManyUserUseCase.execute()
+
+    return users
+  }
+
+  @Delete(":userId")
+  async delete(@Param("userId") userId: string) {
+    await this.deleteUserUseCase.execute({ userId })
+  }
+
+  @Put(":userId")
+  async edit(@Param("userId") userId: string, @Body() body: EditUserBody) {
+    const { name } = body
+    await this.editUserUseCase.execute({
+      name,
+      userId
+    })
+  }
+}
