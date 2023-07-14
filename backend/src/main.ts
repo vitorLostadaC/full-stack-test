dcc7bc8e54bd7de722c406b30b93d42e42add131
vitorLostadaC <vitorlostada@hotmail.com>
2023-07-14 16:22:36 -0300

    feat: change format error message

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
-import { ValidationPipe } from "@nestjs/common"
+import { ValidationError, ValidationPipe } from "@nestjs/common"
+import { IncorrectValuesException } from "./exception/IncorrectValuesException"
+import { mapperClassValidateErrorToAppError } from "./utils/mappers"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors: ValidationError[]) => {
+        throw new IncorrectValuesException(
+          mapperClassValidateErrorToAppError(errors)
+        )
+      }
+    })
+  )
   app.enableCors()
   await app.listen(3000)
 }
