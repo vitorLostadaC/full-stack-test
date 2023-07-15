660fea292ac104e25e9171ac3e0a32aed2596250
vitorLostadaC <vitorlostada@hotmail.com>
2023-07-15 11:01:05 -0300

    feat: configure default styles

---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,26 @@
+import { createTheme } from "@mui/material"
+import { ptBR } from "@mui/material/locale"
+import "@mui/material/styles/createPalette"
+
+export const textFieldHeight = "40px"
+
+export const theme = createTheme(
+  {
+    typography: {
+      fontFamily: "Inter"
+    },
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: `
+            * {
+                margin: 0;
+                padding:0;
+                box-sizing: border-box;
+            }
+           
+            `
+      }
+    }
+  },
+  ptBR
+)
