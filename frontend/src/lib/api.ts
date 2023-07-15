91aa1bc8f3af7958c3961d6634d6a94960d120b9
vitorLostadaC <vitorlostada@hotmail.com>
2023-07-15 13:30:05 -0300

    feat: create interceptorns for api

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,30 @@
-import axios from "axios"
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import axios, { AxiosError, AxiosResponse } from "axios"
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL as string
 })
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("notes.token")
+  if (token) {
+    // TODO: axios bug workaround, ref: https://github.com/axios/axios/issues/5034
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    config.headers = {
+      ...config.headers,
+      Authorization: `Bearer ${token}`
+    }
+  }
+
+  return config
+})
+
+api.interceptors.response.use(
+  (response: AxiosResponse<any>) => {
+    return response.data
+  },
+  (exception: AxiosError) => {
+    return exception.response?.data
+  }
+)
